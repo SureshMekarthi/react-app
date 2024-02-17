adbe7da3b3338b18a17915d4eaea13753bd88959
AnushaPoosala <anusha.poosala@outlook.com>
2024-02-16 23:01:28 -0500

    changes for expandale exersice

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,7 @@ import ExpandableText from "./components/ExpandableText";
 function App() {
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat soluta
-        et recusandae cupiditate necessitatibus asperiores optio eaque quas,
-        animi vero nihil velit est cumque similique hic. Tempore nihil iure
-        velit autem vero hic optio, cupiditate officia unde porro, ducimus non,
-        placeat magnam vel molestiae ut laudantium quaerat exercitationem?
-        Reprehenderit placeat voluptate, perferendis aut consequuntur laborum
-        error aliquam dignissimos ipsam adipisci repudiandae ullam aspernatur
-        excepturi in, velit totam numquam explicabo accusamus ab? Placeat
-        ratione eligendi odit excepturi officiis perferendis dolore voluptatum
-        molestiae odio neque ipsum rem, cupiditate, ipsam provident a quas
-        voluptatibus, voluptate delectus ab officia! Voluptatum accusamus
-        laudantium dolor excepturi?
-      </ExpandableText>
+      <ExpandableText maxChar={10}>Hello world</ExpandableText>
     </div>
   );
 }
--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 interface Props {
   children: string;
+  maxChar?: number;
 }
 
-const ExpandableText = ({ children }: Props) => {
-  return <div>ExpandableText</div>;
+const ExpandableText = ({ children, maxChar = 100 }: Props) => {
+  const [isexpanded, setExpand] = useState(false);
+  if (children.length <= 100) return <p>{children}</p>;
+
+  const text = isexpanded ? children : children.substring(0, maxChar);
+  return (
+    <div>
+      {text}...
+      <button onClick={() => setExpand(!isexpanded)}>
+        {isexpanded ? "less" : "more"}
+      </button>
+    </div>
+  );
 };
 
 export default ExpandableText;
