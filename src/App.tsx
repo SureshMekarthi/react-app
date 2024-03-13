f7597efe8d8bdf05fa7fbed72f7872a11a4b3ce0
AnushaPoosala <anusha.poosala@outlook.com>
2024-03-12 23:27:47 -0400

    added expenses list to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import ExpenseList from "./Expense-Tracker/components/ExpenseList";
 import Form from "./components/Form";
 
 function App() {
+  const expenses = [
+    { id: 1, description: "aaa", amount: 10, category: "utilities" },
+    { id: 1, description: "bbb", amount: 10, category: "utilities" },
+  ];
   return (
     <div>
-      <Form></Form>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => console.log("Delete", id)}
+      ></ExpenseList>
     </div>
   );
 }
