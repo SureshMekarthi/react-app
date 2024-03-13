6b6fd2ab0a395f7c1605a07df66513d101098690
AnushaPoosala <anusha.poosala@outlook.com>
2024-03-12 23:20:30 -0400

    added tofix

--- src/Expense-Tracker/components/ExpenseList.tsx
+++ src/Expense-Tracker/components/ExpenseList.tsx
@@ -40,7 +40,12 @@ const ExpenseList = ({ expenses, onDelete }: Props) => {
       <tfoot>
         <tr>
           <td>Total</td>
-          <td>{expenses.reduce((acc, expense) => expense.amount + acc, 0)}</td>
+          <td>
+            ${" "}
+            {expenses
+              .reduce((acc, expense) => expense.amount + acc, 0)
+              .toFixed(2)}
+          </td>
           <td></td>
           <td></td>
         </tr>
