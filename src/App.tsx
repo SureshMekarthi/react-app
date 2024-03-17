72a6c7d55e9b8bd701e098a79f2195d8c08171f9
AnushaPoosala <anusha.poosala@outlook.com>
2024-03-16 23:56:52 -0400

    delete button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
+import { useState } from "react";
 import ExpenseList from "./Expense-Tracker/components/ExpenseList";
 import Form from "./components/Form";
 
 function App() {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "utilities" },
-    { id: 1, description: "bbb", amount: 10, category: "utilities" },
-  ];
+    { id: 2, description: "bbb", amount: 10, category: "utilities" },
+  ]);
+
   return (
     <div>
       <ExpenseList
         expenses={expenses}
-        onDelete={(id) => console.log("Delete", id)}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       ></ExpenseList>
     </div>
   );
